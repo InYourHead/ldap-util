19da6f3d0a9eea229ed7719c13e7043b1a89d84c
Aleksander Burzec <a.h.burzec@gmail.com>
2024-08-17 10:17:47 +0200

    inital commit

---
+++ src/main/java/com/inyourhead/ldap/ldaputil/LdapUtilApplication.java
@@ -0,0 +1,13 @@
+package com.inyourhead.ldap.ldaputil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inyourhead/ldap/ldaputil/LdapUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyourhead.ldap.ldaputil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
